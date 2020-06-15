bc08bbf229eda7ac306a40a5ce6eb2e5b206a31b
Luiz Serra <lserra.trade@gmail.com>
2020-06-15 20:31:45 -0300

    criando projeto backend e Hello World

---
+++ backend/src/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const { response } = require('express');
+
+app = express();
+
+app.get('/projects', (request, response) => {
+    return response.json({message: 'Hello World'});
+});
+
+app.listen('3333');
