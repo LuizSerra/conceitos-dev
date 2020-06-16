0130f11cd80cf9a955068ad7ef20f198b7e3d7ef
Luiz Serra <lserra.trade@gmail.com>
2020-06-15 22:29:13 -0300

    adicionando metodos http

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,8 +4,41 @@ const { response } = require('express');
 app = express();
 
 app.get('/projects', (request, response) => {
-    return response.json({message: 'Hello World'});
+    return response.json(
+        [  'Projeto 1',
+           'Projeto 2',
+           'Projeto 3',
+           'Projeto 4']
+    );
 });
+
+app.post('/projects', (request, response) => {
+    return response.json(
+        {  'nome'  : 'Site Casas Bahia',
+           'tipo'  : 'E-commerce',
+           'custo' : '50000'
+        }
+    );
+});
+
+app.put('/projects/:id', (request, response) => {
+    return response.json(
+        {  'nome'  : 'Site Casas Bahia',
+           'tipo'  : 'Portal',
+           'custo' : '20000'
+        }
+    );
+});
+
+app.delete('/projects/:id', (request, response) => {
+    return response.json(
+        {  'nome'  : 'Site Casas Bahia',
+           'tipo'  : 'E-commerce',
+           'custo' : '50000'
+        }
+    );
+});
+
 
 app.listen('3333', () => {
     console.log('✈ back-end started!');
