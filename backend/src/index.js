420ac514101cca11cd1fc4f6c6b0bfc0ff2208e1
Luiz Serra <lserra.trade@gmail.com>
2020-06-15 21:48:58 -0300

    instalação do nodemon

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,4 +7,6 @@ app.get('/projects', (request, response) => {
     return response.json({message: 'Hello World'});
 });
 
-app.listen('3333');
+app.listen('3333', () => {
+    console.log('✈ back-end started!');
+});
