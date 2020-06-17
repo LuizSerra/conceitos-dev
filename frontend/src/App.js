ba2e062a52bc415abb50e6eb937694a994e72e2e
Luiz Serra <lserra.trade@gmail.com>
2020-06-17 09:22:31 -0300

    Introdução a propriedades

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,35 @@ import Header from './components/Header';
 function App() {
    return  (
    <>
-   <Header />
-   <Header />
+   <Header title="Homepage">
+       <ul>
+           <li>
+               Homepage
+           </li>
+           <li>
+               Cards
+           </li>
+           <li>
+               Sair
+           </li>
+       </ul>
+   </Header>
+   <Header title="Projects">
+   <ul>
+           <li>
+               Homepage
+           </li>
+           <li>
+               Cards
+           </li>
+           <li>
+               Portfolio
+           </li>
+           <li>
+               Sair
+           </li>
+       </ul>
+   </Header>
    </>
    );
 }
